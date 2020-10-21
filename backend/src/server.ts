2c5bcef1c6c16bf2a7280edf04948e85835dacae
Dyego Soriano <dyegodf2@gmail.com>
2020-10-21 14:25:24 -0300

    configurando express

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,21 @@
-console.log('teste...')
+import dotenv from 'dotenv'
+import app from './app'
+
+dotenv.config()
+
+const port = process.env.PORT || 3333
+
+// notFound
+app.use((request, response, next) => {
+  const error = new Error('Not found')
+  error.status = 404
+  next(error)
+})
+
+// Catch all
+app.use((error, request, response, next) => {
+  response.status(error.status || 500)
+  response.json({ error: error.message })
+})
+
+app.listen(port, console.log(`🚀  Server is running port: ${port}`))
