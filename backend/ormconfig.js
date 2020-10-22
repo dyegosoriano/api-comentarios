c59aa6223465d6c1f480f09f61ed740c1ef5003f
Dyego Soriano <dyegodf2@gmail.com>
2020-10-22 11:33:42 -0300

    config typeorm e variáveis de ambiente

---
+++ backend/ormconfig.js
@@ -0,0 +1,15 @@
+module.exports = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  migrations: ['src/database/migrations/*.ts'],
+  entities: ['src/app/models/*.ts'],
+  synchronize: process.env.DB_SYNC,
+  cli: {
+    migrationsDir: ['src/database/migrations/'],
+    entitiesDir: 'src/app/models/'
+  }
+}
