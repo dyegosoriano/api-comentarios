d2c85c5ca99bff938fb0c2c65f9c6dffbe063016
Dyego Soriano <dyegodf2@gmail.com>
2020-10-23 09:57:57 -0300

    habilitando cors

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import 'reflect-metadata'
+import cors from 'cors'
 
 import routes from './routes'
 import './database'
@@ -13,6 +14,7 @@ class App {
   }
 
   middleware() {
+    this.server.use(cors())
     this.server.use(express.json())
   }
 
